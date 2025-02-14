d3aa6607f55f89db34ef89e4098ee89abd3de4fe
robertGhandi <robertzerah@gmail.com>
2025-02-14 23:37:41 +0100

    config: uvicorn start script

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ chmod -R 777 /var/run/nginx /var/log/nginx
 service nginx start || (cat /var/log/nginx/error.log && exit 1)
 
 # Start FastAPI with Uvicorn
-uvicorn main:app --host 127.0.0.1 --port 8000 --proxy-headers
+uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers
